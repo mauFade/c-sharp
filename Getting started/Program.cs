5955588e2b7e8f229b48fdc66e3621f43fb73909
Mauricio Cardoso <mauricio.cds00@gmail.com>
2022-08-26 22:07:04 -0300

    Adicionar arquivos de projeto.

---
+++ Getting started/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Getting_started
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arrNumbers = new int[] { 1, 2, 3, 4, 5, 6 };
+
+            for(int i = 0; i < arrNumbers.Length; i++)
+            {
+                Console.WriteLine(arrNumbers[i]);
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
